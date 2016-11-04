1396ec74892d10680434c9c158b4c18f97834cc1
Yuval Hermelin <yuval.hermelin@gmail.com>
2016-11-04 10:07:58 -0700

    Final improvements

--- src/com/yuvalhermelin/PeakCounter.java
+++ src/com/yuvalhermelin/PeakCounter.java
@@ -6,7 +6,7 @@ public class PeakCounter {
 	public int countPeaks() {
 		// GyroTest2out.csv
 		// 64StepsInHandJogging-out.csv
-		//walkingSampleData-out.csv
+		// walkingSampleData-out.csv
 		// 64StepsInPocketJogging-out.csv
 		CSVData data = CSVData.readCSVFile("/Users/yuvalhermelin/Downloads/walkingSampleData-out.csv", 0);
 		double[] gyro3D = new double[data.numberOfRows()];
@@ -20,10 +20,17 @@ public class PeakCounter {
 
 		int peaks = 0;
 
+		double[] time = data.getColumn(0);
+		System.out.println(Arrays.toString(time));
+		double previousTime = 0;
+
 		for (int i = 1; i < gyro3D.length - 1; i++)
 			if (gyro3D[i] > gyro3D[i - 1] && gyro3D[i] > gyro3D[i + 1])
-				if (gyro3D[i] > threshold)
+				if (gyro3D[i] > threshold && (previousTime + 300) < time[i]) {
+					System.out.println(previousTime + " " + time[i]);
+					previousTime = time[i];
 					peaks++;
+				}
 		return peaks;
 	}
 
@@ -39,6 +46,22 @@ public class PeakCounter {
 		threshold /= data.length - 1;
 		threshold = Math.sqrt(threshold);
 
-		return (.8*threshold + mean);
+		return (mean + .5 * threshold);
+	}
+
+	public double getAdaptiveThreshold(double[] data, int startingPoint) {
+		double threshold = 0;
+		double mean = 0;
+		for (int i = startingPoint; i < startingPoint + 50; i++)
+			mean += data[i];
+		// Size of the window for the adaptive threshold
+		mean /= 50;
+
+		for (int i = startingPoint; i < startingPoint + 50; i++)
+			threshold += (data[i] - mean) * (data[i] - mean);
+		threshold /= 49;
+		threshold = Math.sqrt(threshold);
+
+		return (mean + .5 * threshold);
 	}
 }
